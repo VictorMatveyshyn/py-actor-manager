e232db5d6a2e91b9a7dc780571b85a07ea7b5089
Victor Matveyshin <vic_m@ukr.net>
2026-05-25 23:17:41 +0300

    first try

--- app/managers.py
+++ app/managers.py
@@ -3,4 +3,41 @@ import sqlite3
 from app.models import Actor
 
 
-# add manager here
+
+class ActorManager:
+    def __init__(self, db_name: str, table_name: str):
+        self.db_name = db_name
+        self.table_name = table_name
+        self._connection = sqlite3.connect(self.db_name)
+        self.cursor = self._connection.cursor()
+
+    def create(self,first_name: str, last_name: str):
+        actor_cursor = self._connection.cursor()
+        actor_cursor.execute(f'INSERT INTO {self.table_name} '
+                             f'(first_name, last_name) VALUES (?, ?)',
+                             (first_name, last_name)
+                             )
+        self._connection.commit()
+
+
+    def all(self):
+        actor_cursor = self._connection.cursor()
+        actor_cursor.execute(f'SELECT * FROM {self.table_name}')
+        return actor_cursor.fetchall()
+
+    def update(self,first_name: str, last_name: str):
+        actor_cursor = self._connection.cursor()
+        actor_cursor.execute(f'UPDATE {self.table_name} '
+                             f'SET first_mane = (?), last_name = (?)',
+                             (first_name, last_name))
+        self._connection.commit()
+
+    def delete(self,first_name: str, last_name: str):
+        actor_cursor = self._connection.cursor()
+        actor_cursor.execute(f'DELETE FROM {self.table_name} '
+                             f'WHERE first_mane = (?) AND last_name = (?)',
+                             (first_name, last_name))
+        self._connection.commit()
+
+# mngr = ActorManager('actor.db', 'actor.sqlite3')
+# print(mngr.all())
