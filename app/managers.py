7be6f72fbe136d9b3d0ea6de76352f5d5c7d34eb
Victor Matveyshin <vic_m@ukr.net>
2026-05-26 21:01:26 +0300

    repeat again 23

--- app/managers.py
+++ app/managers.py
@@ -4,44 +4,44 @@ from app.models import Actor
 
 
 class ActorManager:
-    def __init__(self, db_name: str, table_name: str = "actors"):
+    def __init__(self, db_name: str, table_name: str = "actors") -> None:
         self.db_name = db_name
         self.table_name = table_name
         self._connection = sqlite3.connect(self.db_name)
         self.cursor = self._connection.cursor()
-        self.cursor.execute(f'CREATE TABLE IF NOT EXISTS {self.table_name} '
-                            f'( id INTEGER PRIMARY KEY, '
-                            f'first_name TEXT NOT NULL, '
-                            f'last_name TEXT NOT NULL ) ')
+        self.cursor.execute(f"CREATE TABLE IF NOT EXISTS {self.table_name} "
+                            f"( id INTEGER PRIMARY KEY, "
+                            f"first_name TEXT NOT NULL, "
+                            f"last_name TEXT NOT NULL ) ")
         self._connection.commit()
 
-    def create(self,first_name: str, last_name: str):
+    def create(self,first_name: str, last_name: str) -> None:
         actor_cursor = self._connection.cursor()
-        actor_cursor.execute(f'INSERT INTO {self.table_name} '
-                             f'(first_name, last_name) VALUES (?, ?)',
+        actor_cursor.execute(f"INSERT INTO {self.table_name} "
+                             f"(first_name, last_name) VALUES (?, ?)",
                              (first_name, last_name)
                              )
         self._connection.commit()
 
-    def all(self):
+    def all(self) -> list[Actor]:
         actor_cursor = self._connection.cursor()
-        actor_cursor.execute(f'SELECT * FROM {self.table_name}')
+        actor_cursor.execute(f"SELECT * FROM {self.table_name}")
         rows = actor_cursor.fetchall()
-        return [Actor(id=row[0], first_name=row[1], last_name=row[2]) for row in rows]
+        return [Actor(id=row[0],
+                      first_name=row[1],
+                      last_name=row[2]) for row in rows]
 
-    def update(self, pk: int, new_first_name: str, new_last_name: str):
+    def update(self, pk: int, new_first_name: str, new_last_name: str) -> None:
         actor_cursor = self._connection.cursor()
-        actor_cursor.execute(f'UPDATE {self.table_name} '
-                             f'SET first_name = ?, last_name = ? '
-                             f'WHERE id = ?',
+        actor_cursor.execute(f"UPDATE {self.table_name} "
+                             f"SET first_name = ?, last_name = ? "
+                             f"WHERE id = ?",
                              (new_first_name, new_last_name, pk))
         self._connection.commit()
 
-    def delete(self, pk: int):
+    def delete(self, pk: int) -> None:
         actor_cursor = self._connection.cursor()
-        actor_cursor.execute(f'DELETE FROM {self.table_name} '
-                             f'WHERE id = ?', (pk, )
+        actor_cursor.execute(f"DELETE FROM {self.table_name} "
+                             f"WHERE id = ?", (pk, )
                              )
         self._connection.commit()
-
-
